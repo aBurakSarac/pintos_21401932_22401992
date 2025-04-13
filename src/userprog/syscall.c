c6ef1d8b99ee7f4feedad45323bd94a3b14966f0
aBurakSarac <alibsarac@gmail.com>
2025-04-13 15:26:30 +0300

    Feature: SYS_WAIT and SYS_CREATE implemented

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -21,13 +21,54 @@ static void syscall_handler (struct intr_frame *f) {
   int syscall_number;
   check_user_address(f->esp);
   syscall_number = *(int *)f->esp;
-  
-  if(syscall_number==SYS_EXIT){
+  if (syscall_number == SYS_HALT) {
+    //passes tests?
+  }
+  else if(syscall_number==SYS_EXIT){
     int status;
     check_user_address(f->esp + sizeof(int));
     status = *(int *)(f->esp + sizeof(int));
     thread_current()->exit_code = status;
     thread_exit();
+  }
+  else if (syscall_number==SYS_EXEC){
+    const char *cmd_line;
+    check_user_address(f->esp + sizeof(int));
+    cmd_line = *(const char **)(f->esp + sizeof(int));
+    check_user_address(cmd_line);
+    f->eax = process_execute(cmd_line);
+  }
+  else if(syscall_number==SYS_WAIT){
+    int child_tid;
+    check_user_address(f->esp + sizeof(int));
+    child_tid = *(int *)(f->esp + sizeof(int));
+    f->eax = process_wait(child_tid);
+  }
+  else if(syscall_number==SYS_CREATE){
+    const char *file_name;
+    unsigned initial_size;
+        
+    check_user_address(f->esp + sizeof(int));
+    file_name = *(const char **)(f->esp + sizeof(int));
+        
+    check_user_address(f->esp + 2 * sizeof(int));
+    initial_size = *(unsigned *)(f->esp + 2 * sizeof(int));
+
+    check_user_address(file_name);
+        
+    f->eax = filesys_create(file_name, initial_size);
+  }
+  else if(syscall_number==SYS_REMOVE){
+    
+  }
+  else if(syscall_number==SYS_OPEN){
+    
+  }
+  else if(syscall_number==SYS_FILESIZE){
+    
+  }
+  else if(syscall_number==SYS_READ){
+    
   }
   else if(syscall_number==SYS_WRITE){
     int fd;
@@ -50,11 +91,18 @@ static void syscall_handler (struct intr_frame *f) {
 
     f->eax = size;
   }
-  else if(syscall_number==SYS_EXEC){
-    check_user_address(f->esp + sizeof(int));
-    char *cmd = *(char **)(f->esp + sizeof(int));
-    check_user_address(cmd);
-    f->eax = process_execute(cmd);
+  else if(syscall_number==SYS_SEEK){
+    
+  }
+  else if(syscall_number==SYS_TELL){
+    
+  }
+  else if(syscall_number==SYS_CLOSE){
+    
+  }
+  else{
+    thread_current()->exit_code = -1;
+    thread_exit();
   }
 }
 
