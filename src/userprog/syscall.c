30718ecf208e101e40942b6cf3bd504b0672e585
smpilaz <pilazsudemelis@gmail.com>
2025-04-15 18:21:21 +0300

    Fix: switch case

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -32,161 +32,187 @@ static void syscall_handler (struct intr_frame *f) {
   int syscall_number;
   check_user_address(f->esp);
   syscall_number = *(int *)f->esp;
-  if (syscall_number == SYS_HALT) {
-    shutdown_power_off();
-  }
-  else if(syscall_number==SYS_EXIT){
-    int status;
-    check_user_address(f->esp + 4);
-    status = *(int *)(f->esp + 4);
-    thread_current()->exit_code = status;
-    thread_exit();
-  }
-  else if (syscall_number==SYS_EXEC){
-    const char *cmd_line;
-    check_user_address(f->esp + 4);
-    cmd_line = *(const char **)(f->esp + 4);
-    check_user_address(cmd_line);
-    f->eax = process_execute(cmd_line);
-  }
-  else if(syscall_number==SYS_WAIT){
-    int child_tid;
-    check_user_address(f->esp + 4);
-    child_tid = *(int *)(f->esp + 4);
-    f->eax = process_wait(child_tid);
-  }
-  else if(syscall_number==SYS_CREATE){
-    const char *file_name;
-    unsigned initial_size;
-        
-    check_user_address(f->esp + 4);
-    file_name = *(const char **)(f->esp + 4);
-        
-    check_user_address(f->esp + 8);
-    initial_size = *(unsigned *)(f->esp + 8);
+  switch (syscall_number) {
+    case SYS_HALT:
+      shutdown_power_off();
+      break;
 
-    check_user_address(file_name);
-        
-    f->eax = filesys_create(file_name, initial_size);
-  }
-  else if(syscall_number==SYS_REMOVE){
-    check_user_address(f->esp + 4);
-    const char *file_name = *(const char **)(f->esp + 4);
-    check_user_address(file_name);
-    lock_acquire(&file_lock);
-    f->eax = sys_remove(file_name);
-    lock_release(&file_lock);
-  }
-  else if(syscall_number==SYS_OPEN){
-    char *file_name = *((char **)(f->esp + 4));
-    check_user_address(file_name);
-    int fd = sys_open(file_name);
-    f->eax = fd; 
-  }
-  else if(syscall_number==SYS_FILESIZE){
-    int fd = *(int *)(f->esp + 4);
-    struct file *f_obj = get_file_by_fd(fd);
-    if (f_obj == NULL) {
-      f->eax = -1;
-    } else {
-      f->eax = file_length(f_obj);
+    case SYS_EXIT: {
+      int status;
+      check_user_address(f->esp + 4);
+      status = *(int *)(f->esp + 4);
+      thread_current()->exit_code = status;
+      thread_exit();
+      break;
     }
-  }
-  else if(syscall_number==SYS_READ){
-    int fd = *((int *)(f->esp + 4));
-    void *buffer = *((void **)(f->esp + 8));
-    unsigned size = *((unsigned *)(f->esp + 12));
-    check_user_address(buffer);
 
-    int read = sys_read(fd, buffer, size);
-    f->eax = read;
-    
-  }
-  else if(syscall_number==SYS_WRITE){
-    int fd;
-    const void *buffer;
-    unsigned size;
-        
-    check_user_address(f->esp + 4);
-    fd = *(int *)(f->esp + 4);
-        
-    check_user_address(f->esp + 8);
-    buffer = *(const void **)(f->esp + 8);
-        
-    check_user_address(f->esp + 12);
-    size = *(unsigned *)(f->esp + 12);  
+    case SYS_EXEC: {
+      const char *cmd_line;
+      check_user_address(f->esp + 4);
+      cmd_line = *(const char **)(f->esp + 4);
+      check_user_address(cmd_line);
+      f->eax = process_execute(cmd_line);
+      break;
+    }
 
-    check_user_address(buffer);
-    if (fd == 1) {
+    case SYS_WAIT: {
+      int child_tid;
+      check_user_address(f->esp + 4);
+      child_tid = *(int *)(f->esp + 4);
+      f->eax = process_wait(child_tid);
+      break;
+    }
+
+    case SYS_CREATE: {
+      const char *file_name;
+      unsigned initial_size;
+
+      check_user_address(f->esp + 4);
+      file_name = *(const char **)(f->esp + 4);
+
+      check_user_address(f->esp + 8);
+      initial_size = *(unsigned *)(f->esp + 8);
+
+      check_user_address(file_name);
+
+      f->eax = filesys_create(file_name, initial_size);
+      break;
+    }
+
+    case SYS_REMOVE: {
+      check_user_address(f->esp + 4);
+      const char *file_name = *(const char **)(f->esp + 4);
+      check_user_address(file_name);
       lock_acquire(&file_lock);
-      putbuf(buffer, size);
+      f->eax = sys_remove(file_name);
       lock_release(&file_lock);
-      f->eax = size;
-    }else {
-      struct file_descriptor *temp =NULL;
+      break;
+    }
+
+    case SYS_OPEN: {
+      char *file_name = *((char **)(f->esp + 4));
+      check_user_address(file_name);
+      int fd = sys_open(file_name);
+      f->eax = fd;
+      break;
+    }
+
+    case SYS_FILESIZE: {
+      int fd = *(int *)(f->esp + 4);
+      struct file *f_obj = get_file_by_fd(fd);
+      if (f_obj == NULL) {
+        f->eax = -1;
+      } else {
+        f->eax = file_length(f_obj);
+      }
+      break;
+    }
+
+    case SYS_READ: {
+      int fd = *((int *)(f->esp + 4));
+      void *buffer = *((void **)(f->esp + 8));
+      unsigned size = *((unsigned *)(f->esp + 12));
+      check_user_address(buffer);
+
+      int read = sys_read(fd, buffer, size);
+      f->eax = read;
+      break;
+    }
+
+    case SYS_WRITE: {
+      int fd;
+      const void *buffer;
+      unsigned size;
+
+      check_user_address(f->esp + 4);
+      fd = *(int *)(f->esp + 4);
+
+      check_user_address(f->esp + 8);
+      buffer = *(const void **)(f->esp + 8);
+
+      check_user_address(f->esp + 12);
+      size = *(unsigned *)(f->esp + 12);
+
+      check_user_address(buffer);
+      if (fd == 1) {
+        lock_acquire(&file_lock);
+        putbuf(buffer, size);
+        lock_release(&file_lock);
+        f->eax = size;
+      } else {
+        struct file_descriptor *temp = NULL;
+        struct list_elem *e;
+        lock_acquire(&file_lock);
+        for (e = list_begin(&thread_current()->open_files); e != list_end(&thread_current()->open_files); e = list_next(e)) {
+          temp = list_entry(e, struct file_descriptor, elem);
+          if (temp->file_id == fd) {
+            f->eax = file_write(temp->file, buffer, size);
+            break;
+          }
+        }
+        lock_release(&file_lock);
+        if (temp == NULL) {
+          f->eax = -1;
+        }
+      }
+      break;
+    }
+
+    case SYS_SEEK: {
+      check_user_address(f->esp + 4);
+      int fd = *(int *)(f->esp + 4);
+      check_user_address(f->esp + 8);
+      unsigned position = *(unsigned *)(f->esp + 8);
+      struct file *file = get_file_by_fd(fd);
+      if (file != NULL) {
+        lock_acquire(&file_lock);
+        file_seek(file, position);
+        lock_release(&file_lock);
+      }
+      break;
+    }
+
+    case SYS_TELL: {
+      check_user_address(f->esp + 4);
+      int fd = *(int *)(f->esp + 4);
+      struct file *file = get_file_by_fd(fd);
+      if (file != NULL) {
+        lock_acquire(&file_lock);
+        f->eax = file_tell(file);
+        lock_release(&file_lock);
+      } else {
+        f->eax = -1;
+      }
+      break;
+    }
+
+    case SYS_CLOSE: {
+      check_user_address(f->esp + 4);
+      int fd = *(int *)(f->esp + 4);
+      struct thread *cur = thread_current();
       struct list_elem *e;
+      struct file_descriptor *fd_struct = NULL;
+
       lock_acquire(&file_lock);
-      for (e = list_begin(&thread_current()->open_files); e != list_end(&thread_current()->open_files); e = list_next(e)) {
-        temp = list_entry(e, struct file_descriptor, elem);
-        if (temp->file_id == fd) {
-          f->eax = file_write(temp->file, buffer, size);
+      for (e = list_begin(&cur->open_files); e != list_end(&cur->open_files); e = list_next(e)) {
+        fd_struct = list_entry(e, struct file_descriptor, elem);
+        if (fd_struct->file_id == fd) {
+          list_remove(&fd_struct->elem);
           break;
         }
       }
-      lock_release(&file_lock);
-      if (temp == NULL) {
-        f->eax = -1; 
+      if (fd_struct != NULL) {
+        file_close(fd_struct->file);
+        free(fd_struct);
       }
-    }
-  }
-  else if(syscall_number==SYS_SEEK){
-    check_user_address(f->esp + 4);
-    int fd = *(int *)(f->esp + 4);
-    check_user_address(f->esp + 8);
-    unsigned position = *(unsigned *)(f->esp + 8);
-    struct file *file = get_file_by_fd(fd);
-    if (file != NULL) {
-      lock_acquire(&file_lock);
-      file_seek(file, position);
       lock_release(&file_lock);
+      break;
     }
-  }
-  else if(syscall_number==SYS_TELL){
-    check_user_address(f->esp + 4);
-    int fd = *(int *)(f->esp + 4);
-    struct file *file = get_file_by_fd(fd);
-    if (file != NULL) {
-      lock_acquire(&file_lock);
-      f->eax = file_tell(file);
-      lock_release(&file_lock);
-    } else {
-      f->eax = -1;
-    }
-  }
-  else if(syscall_number==SYS_CLOSE){
-    check_user_address(f->esp + 4);
-    int fd = *(int *)(f->esp + 4);
-    struct thread *cur = thread_current();
-    struct list_elem *e;
-    struct file_descriptor *fd_struct = NULL;
-    
-    lock_acquire(&file_lock);
-    for (e = list_begin(&cur->open_files); e != list_end(&cur->open_files); e = list_next(e)) {
-      fd_struct = list_entry(e, struct file_descriptor, elem);
-      if (fd_struct->file_id == fd) {
-        list_remove(&fd_struct->elem);
-        break;
-      }
-    }
-    if (fd_struct != NULL) {
-      file_close(fd_struct->file);
-      free(fd_struct);
-    }
-    lock_release(&file_lock);
-  }
-  else{
-    thread_current()->exit_code = -1;
-    thread_exit();
+
+    default:
+      thread_current()->exit_code = -1;
+      thread_exit();
+      break;
   }
 }
 
